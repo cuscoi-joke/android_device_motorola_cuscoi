62c0c1d856a2f707806d8711016a80ae08e37818
Rakhshan Ali <alirakhshan7070@gmail.com>
2025-06-03 19:10:59 +0530

    [TMP] cuscoi: Don't build sensors hal
    - Also don't don't copy hals.conf, it's present in proprietary files.

--- device.mk
+++ device.mk
@@ -63,11 +63,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
 # Sensors
-PRODUCT_PACKAGES += \
-    sensors.cuscoi
+#PRODUCT_PACKAGES += \
+#    sensors.cuscoi
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+#PRODUCT_COPY_FILES += \
+#    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
